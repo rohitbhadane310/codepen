d90722c726fe3581170be181d76831cd09c1cbca
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-01-02 19:48:56 +0530

    reset project

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './components/App';
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);  
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
