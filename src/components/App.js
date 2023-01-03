35fbb69a1edf32e2a81eb083e5425b93f7be8214
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-01-03 11:05:42 +0530

    complete app

--- src/components/App.js
+++ src/components/App.js
@@ -1,55 +1,60 @@
-import React, { useState } from "react";
-import Editor from "./Editor";
+import React, { useState, useEffect } from 'react';
+import Editor from './Editor'
+import useLocalStorage from '../hooks/useLocalStorage'
 
-function App(){
+function App() {
+  const [html, setHtml] = useLocalStorage('html', '')
+  const [css, setCss] = useLocalStorage('css', '')
+  const [js, setJs] = useLocalStorage('js', '')
+  const [srcDoc, setSrcDoc] = useState('')
 
-  const [html , setHtml] = useState('')
-  const [css , setCss] = useState('')
-  const [js , setJs] = useState('')
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    }, 250)
 
+    return () => clearTimeout(timeout)
+  }, [html, css, js])
 
-  const srcDoc = `
-   <html>
-      <body>${html}</body>
-      <style>${html}</style>
-      <script>${js}</script>
-   </html>
-  `
-
-
-  return(
-   <>
-     <div className="pane top-pane">
-      <Editor 
-         language = "xml"
-         displayName = "HTML"
-         value={html}
-         onChange={setHtml}
-      />
-      <Editor
-          language = "css"
-          displayName = "CSS"
+  return (
+    <>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language="css"
+          displayName="CSS"
           value={css}
           onChange={setCss}
-      />
-      <Editor
-         language = "javascript"
-         displayName = "JS"
-         value={js}
-         onChange={setJs}
-      />
-     </div>
-     <div className="pane">
-      <iframe
-        srcDoc={srcDoc}
-        title="output"
-        sandbox="allow-scripts"
-        frameBorder="0"
-        width="100%"
-        height="100%"
-      />
-     </div>
-   </>
+        />
+        <Editor
+          language="javascript"
+          displayName="JS"
+          value={js}
+          onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
+    </>
   )
 }
 
--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,31 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/material.css'
 import 'codemirror/mode/xml/xml'
 import 'codemirror/mode/javascript/javascript'
 import 'codemirror/mode/css/css'
 import { Controlled as ControlledEditor } from 'react-codemirror2'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons'
 
 export default function Editor(props) {
-
-  const{
+  const {
     language,
     displayName,
     value,
     onChange
   } = props
+  const [open, setOpen] = useState(true)
 
-  function handleChange(editor , data , value){
-
+  function handleChange(editor, data, value) {
     onChange(value)
   }
 
-
   return (
-    <div className='editor-container'>
-      <div className='editor-title'>
+    <div className={`editor-container ${open ? '' : 'collapsed'}`}>
+      <div className="editor-title">
         {displayName}
-        <button>O/C</button>
+        <button
+          type="button"
+          className="expand-collapse-btn"
+          onClick={() => setOpen(prevOpen => !prevOpen)}
+        >
+          <FontAwesomeIcon icon={open ? faCompressAlt : faExpandAlt} />
+        </button>
       </div>
       <ControlledEditor
         onBeforeChange={handleChange}
@@ -36,7 +42,7 @@ export default function Editor(props) {
           lint: true,
           mode: language,
           theme: 'material',
-          lineNumbers: true 
+          lineNumbers: true
         }}
       />
     </div>
